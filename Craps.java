fc03e7bb114aea77874ce48ce97d53683b17e33d
Richard White <rwhite@crashwhite.com>
2021-02-04 12:54:57 -0800

    First commit

---
+++ Craps.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Craps here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Craps
+{
+    public static void main(String[] args)
+    {
+        
+        
+    }
+}
