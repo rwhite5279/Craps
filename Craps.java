5d068517e31e823932c61c595da805fea83f67c9
Richard White <rwhite@crashwhite.com>
2021-02-04 13:47:09 -0800

    Complete the main game logic (still needs to be tested)

--- Craps.java
+++ Craps.java
@@ -9,7 +9,38 @@ public class Craps
 {
     public static void main(String[] args)
     {
+        Die d1 = new Die();
+        Die d2 = new Die();
         
         
+        int total = d1.roll() + d2.roll();
+        if (total == 7 || total == 11)
+        {
+            // won the game!
+        }
+        else if (total == 2 || total == 3 || total == 12)
+        {
+            // lost the game!
+        }
+        else     // didn't win or lose, try to roll point
+        {
+            int point = total;
+            total = d1.roll() + d2.roll();
+            while (total != 7 && total != point)
+            {
+                // haven't won or lost yet
+                total = d1.roll() + d2.roll();
+                
+            }
+            // Give the right message
+            if (total == point)
+            {
+                // won the game!
+            }
+            else
+            {
+                // lost the game!
+            }
+        }
     }
 }
