e6f1ad40c88af2721c83f070a7eff6daa78a42ce
Richard White <rwhite@crashwhite.com>
2021-02-04 13:50:04 -0800

    This version doesn't work

--- Craps.java
+++ Craps.java
@@ -10,7 +10,7 @@ public class Craps
     public static void main(String[] args)
     {
         Die d1 = new Die();
-        Die d2 = new Die();
+        
         
         
         int total = d1.roll() + d2.roll();
@@ -25,11 +25,11 @@ public class Craps
         else     // didn't win or lose, try to roll point
         {
             int point = total;
-            total = d1.roll() + d2.roll();
+            
             while (total != 7 && total != point)
             {
                 // haven't won or lost yet
-                total = d1.roll() + d2.roll();
+                
                 
             }
             // Give the right message
