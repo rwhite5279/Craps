0d77a6a81d9b701ff21bf47850941b905efcb42f
Richard White <rwhite@crashwhite.com>
2021-02-08 14:19:30 -0800

    Added a second die to the game

--- Craps.java
+++ Craps.java
@@ -10,7 +10,7 @@ public class Craps
     public static void main(String[] args)
     {
         Die d1 = new Die();
-        
+        Die d2 = new Die();
         
         
         int total = d1.roll() + d2.roll();
